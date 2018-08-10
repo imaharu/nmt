f56e5dfaf07e63b07a531af45530641029c204ba
imaharu <light1200out@gmail.com>
2018-08-10 13:30:24 +0900

    GPUをCPUに変更

--- encoder_decoder/batch_learning.py
+++ encoder_decoder/batch_learning.py
@@ -7,6 +7,8 @@ import chainer.links as L
 import MeCab
 import re
 import time
+import numpy as np
+
 path_train_en = "/home/ochi/src/data/train/train_clean.txt.en"
 path_train_ja = "/home/ochi/src/data/train/train_clean.txt.ja"
 train_num = 200
@@ -70,18 +72,21 @@ class MyMT(chainer.Chain):
             linear1 = L.Linear(k, jv),
         )
 
-    def __call__(self, batch_inputs ,batch_targets):
+    def __call__(self, input_line ,target_line):
         global accum_loss
+        print(input_line[0])
 
-        print("batch_inputs",batch_inputs)
+        for sentence_words in input_line:
+            input_k = self.embed_input(sentence_words)
+            h = self.lstm1(input_k)
 
         for i in range(len(input_line)):
             wid = input_vocab[input_line[i]]
-            input_k = self.embed_input(Variable(xp.array([wid], dtype=xp.int32)))
+            input_k = self.embed_input(Variable(np.array([wid], dtype=np.int32)))
             h = self.lstm1(input_k)
 
-        last_input_k = self.embed_input(Variable(xp.array([input_vocab["<eos>"]],dtype=xp.int32)))
-        tx = Variable(xp.array([input_vocab[input_line[0]]], dtype=xp.int32))
+        last_input_k = self.embed_input(Variable(np.array([input_vocab["<eos>"]],dtype=np.int32)))
+        tx = Variable(np.array([input_vocab[input_line[0]]], dtype=np.int32))
         
         h = self.lstm1(last_input_k)
 
@@ -89,9 +94,9 @@ class MyMT(chainer.Chain):
 
         for i in range(len(target_line)):
             wid = target_vocab[target_line[i]]
-            target_k = self.embed_target(Variable(xp.array([wid], dtype=xp.int32)))
+            target_k = self.embed_target(Variable(np.array([wid], dtype=np.int32)))
             next_wid = target_vocab["<eos>"] if (i == len(target_line) -1) else target_vocab[target_line[i+1]]
-            tx = Variable(xp.array([next_wid], dtype=xp.int32))
+            tx = Variable(np.array([next_wid], dtype=np.int32))
             h = self.lstm1(target_k)
             
             loss = F.softmax_cross_entropy(self.linear1(h),tx)
@@ -99,13 +104,13 @@ class MyMT(chainer.Chain):
         return accum_loss
 
 demb = 64
-batch_size = 5
+batch_size = 1
 model = MyMT(ev, jv, demb)
 
-gpu_device = 0
-cuda.get_device(gpu_device).use()
-model.to_gpu()
-xp = cuda.cupy
+# gpu_device = 0
+# cuda.get_device(gpu_device).use()
+# model.to_gpu()
+# np = cuda.cupy
 # optimizer = optimizers.Adam()
 optimizer = optimizers.SGD()
 optimizer.setup(model)
@@ -113,11 +118,11 @@ optimizer.setup(model)
 def padding(batch_lines):
     for i in range(len(batch_lines)):
         if i == 0:
-            a1 = xp.array([ batch_lines[i] ], dtype=xp.int32)
+            a1 = np.array([ batch_lines[i] ], dtype=np.int32)
             batch_padding = F.pad_sequence(a1 ,50 ,-1)
         else:
-            a1 = xp.array([ batch_lines[i] ], dtype=xp.int32)
-            k = F.pad_sequence(a1,50,-1)
+            a1 = np.array([ batch_lines[i] ], dtype=np.int32)
+            k = F.pad_sequence(a1, 50, -1)
             batch_padding = F.concat((batch_padding, k), axis=0)
     return batch_padding
 
@@ -125,38 +130,36 @@ def reStructured(batch_input_paddings):
     for i in range(50): # padding_num　実際は
         for j in range(batch_size):
             if j == 0:
-                word_line = xp.array([ batch_input_paddings[j][i].data ], dtype=xp.int32)
+                word_line = np.array([ batch_input_paddings[j][i].data ], dtype=np.int32)
             else:
-                a = xp.array([ batch_input_paddings[j][i].data ], dtype=xp.int32)
+                a = np.array([ batch_input_paddings[j][i].data ], dtype=np.int32)
                 word_line = F.concat((word_line, a), axis=0)
         if i == 0:
-            reStructured = xp.array([ word_line.data ], dtype=xp.int32)
+            reStructured = np.array([ word_line.data ], dtype=np.int32)
         else:
-            a = xp.array([ word_line.data ], dtype=xp.int32)
+            a = np.array([ word_line.data ], dtype=np.int32)
             reStructured = F.concat((reStructured, a), axis=0)
     return reStructured
 
 start = time.time()
 for epoch in range(1):
     print("epoch",epoch)
-    indexes = xp.random.permutation(train_num)
+    indexes = np.random.permutation(train_num)
 
     for i in range(0, train_num, batch_size):
         batch_input_lines = [ input_lines_number[int(index)] for index in indexes[i:i+batch_size]]
         batch_input_paddings = padding(batch_input_lines)
         input_reStructured = reStructured(batch_input_paddings)
-
         batch_target_lines = [ target_lines_number[int(index)] for index in indexes[i:i+batch_size]]
         batch_target_paddings = padding(batch_target_lines)
         target_reStructured = reStructured(batch_target_paddings)
 
-
-        # model.lstm1.reset_state()
-        # model.cleargrads()
-        # loss = model(batch_input, batch_target)
-        # loss.backward()
-        # loss.unchain_backward()
-        # optimizer.update()
+        model.lstm1.reset_state()
+        model.cleargrads()
+        loss = model(input_reStructured, target_reStructured)
+        loss.backward()
+        loss.unchain_backward()
+        optimizer.update()
         break
     # outfile = "mt-" + str(epoch) + ".model"
     # serializers.save_npz(outfile, model)
