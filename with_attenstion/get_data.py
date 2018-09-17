0699313fc9fe4dfaf45cd8fc068710567afe73de
imaharu <light1200out@gmail.com>
2018-09-17 17:26:25 +0900

    First commit with_attention

---
+++ with_attenstion/get_data.py
@@ -0,0 +1,58 @@
+def get_train_data_input(train_num, input_vocab, input_lines_number, input_lines):
+    with open("/home/ochi/src/data/train/train_clean.txt.en",'r',encoding='utf-8') as f:
+        lines_en = f.read().strip().split('\n')
+        i = 0
+        for line in lines_en:
+            if i == train_num:
+                break
+            for input_word in line.split():
+                if input_word not in input_vocab:
+                    input_vocab[input_word] = len(input_vocab) + 1
+            input_lines_number[i] = [input_vocab[word] for word in line.split()]
+            input_lines[i] = line
+            i += 1
+        input_vocab['<eos>'] = len(input_vocab)
+
+def get_train_data_target(train_num, target_vocab, target_lines_number, target_lines, translate_words):
+    with open("/home/ochi/src/data/train/train_clean.txt.ja",'r',encoding='utf-8') as f:
+        lines_ja = f.read().strip().split('\n')
+        i = 0
+        for line in lines_ja:
+            if i == train_num:
+                break
+            for target_word in line.split():
+                if target_word not in target_vocab:
+                    id = len(target_vocab) + 1
+                    target_vocab[target_word] = len(target_vocab) + 1
+                    translate_words[id] = target_word
+            target_lines_number[i] = [target_vocab[word] for word in line.split()]
+            target_lines[i] = line
+            i += 1
+
+        id = len(target_vocab)
+        target_vocab['<eos>'] = id
+        translate_words[id] = "<eos>"
+
+def get_test_data_target(test_num, test_input_lines):
+    with open("/home/ochi/src/data/test/test_clean.txt.en",'r',encoding='utf-8') as f:
+        lines_en = f.read().strip().split('\n')
+        i = 0
+        for line in lines_en:
+            if i == test_num:
+                break
+            test_input_lines[i] = line
+            i += 1
+
+# Only Pytorch
+import torch.nn.functional as F
+from torch import tensor as tt
+import torch
+
+def Padding(batch_lines, padding_num):
+    for i in range(len(batch_lines)):
+        if i == 0:
+            batch_padding = F.pad(tt( [ batch_lines[i] ] ) , (0, padding_num - len(batch_lines[i])), mode='constant', value=0)
+        else:
+            k = F.pad(tt( [ batch_lines[i] ] ) , (0, padding_num - len(batch_lines[i])), mode='constant', value=0)
+            batch_padding = torch.cat((batch_padding, k), dim=0)
+    return batch_padding
---
+++ with_attenstion/main.py
@@ -0,0 +1,92 @@
+import torch
+from torch import tensor as tt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.nn.utils.rnn import *
+import time
+import numpy as np
+from get_data import *
+import torch.optim as optim
+
+train_num, padding_num, hidden_size, batch_size = 20000, 50, 256, 50
+
+input_vocab , input_lines, input_lines_number = {}, {}, {}
+target_vocab ,target_lines ,target_lines_number = {}, {}, {}
+translate_words = {}
+
+get_train_data_input(train_num, input_vocab, input_lines_number, input_lines)
+ev = len(input_vocab)
+
+get_train_data_target(train_num, target_vocab, target_lines_number, target_lines, translate_words)
+jv = len(target_vocab)
+
+class Encoder_Decoder(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size):
+        super(Encoder_Decoder, self).__init__()
+        self.embed_input = nn.Embedding(input_size, hidden_size, padding_idx=0)
+        self.embed_target = nn.Embedding(output_size, hidden_size, padding_idx=0)
+
+        self.lstm1 = nn.LSTMCell(hidden_size, hidden_size)
+        self.linear1 = nn.Linear(hidden_size, output_size)
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+    def forward(self, input_lines ,target_lines):
+        global all_loss
+
+        hx = torch.zeros(batch_size, self.hidden_size).cuda()
+        cx = torch.zeros(batch_size, self.hidden_size).cuda()
+
+        for input_sentence_words in input_lines:
+            input_k = self.embed_input(input_sentence_words)
+            hx, cx = self.lstm1(input_k, (hx, cx) )
+        
+        target_lines_not_last = target_lines[:(padding_num-1)]
+        target_lines_next = target_lines[1:]
+        for i ,(target_sentence_words , target_sentence_words_next) in enumerate(zip(target_lines_not_last, target_lines_next)):
+            if i == 0:
+                accum_loss = F.cross_entropy(self.linear1(hx), target_sentence_words)
+            target_k = self.embed_target(target_sentence_words)
+            hx, cx = self.lstm1(target_k, (hx, cx) )
+            loss = F.cross_entropy(self.linear1(hx), target_sentence_words_next)
+            accum_loss += loss
+
+        return accum_loss
+
+model = Encoder_Decoder(ev, jv, hidden_size)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+device = torch.device('cuda:0')
+model = model.to(device)
+
+start = time.time()
+
+epoch_num = 20
+for epoch in range(epoch_num):
+    print("epoch",epoch)
+    indexes = torch.randperm(train_num)
+    for i in range(0, train_num, batch_size):
+        batch_input_lines = [ input_lines_number[int(index)] for index in indexes[i:i+batch_size]]
+        for batch_input_line in batch_input_lines:
+            batch_input_line.append(input_vocab['<eos>'])
+        batch_input_paddings = Padding(batch_input_lines, padding_num)
+        Transposed_input = batch_input_paddings.t().cuda()
+
+        batch_target_lines = [ target_lines_number[int(index)] for index in indexes[i:i+batch_size]]
+        for batch_target_line in batch_target_lines:
+            batch_target_line.append(target_vocab['<eos>'])
+        batch_target_paddings = Padding(batch_target_lines, padding_num)
+        Transposed_target = batch_target_paddings.t().cuda()
+
+        optimizer.zero_grad()
+        loss = model(Transposed_input, Transposed_target)
+        loss.backward()
+        optimizer.step()
+
+    # if (epoch + 1) % 5 == 0:
+    #     outfile = "gpu_batch_mt-" + str(epoch) + ".model"
+    #     torch.save(model.state_dict(), outfile)
+    elapsed_time = time.time() - start
+    print("時間:",elapsed_time / 60.0, "分")
