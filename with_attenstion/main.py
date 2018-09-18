10b072e6d6d9cb98cadb1718a18e4e5459ce6231
imaharu <light1200out@gmail.com>
2018-09-18 17:32:14 +0900

    Done at

--- with_attenstion/main.py
+++ with_attenstion/main.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.nn.utils.rnn import *
 import time
-import numpy as np
 from get_data import *
 import torch.optim as optim
+import math
 
-train_num, padding_num, hidden_size, batch_size = 20000, 50, 256, 50
+train_num, padding_num, hidden_size, batch_size = 5, 50, 256, 5
 
 input_vocab , input_lines, input_lines_number = {}, {}, {}
 target_vocab ,target_lines ,target_lines_number = {}, {}, {}
@@ -29,29 +29,53 @@ class Encoder_Decoder(nn.Module):
 
         self.lstm1 = nn.LSTMCell(hidden_size, hidden_size)
         self.linear1 = nn.Linear(hidden_size, output_size)
+        self.target_linear1 = nn.Linear(hidden_size + hidden_size, output_size)
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
+    def each_score(self, ht, hs):
+        bmm = torch.bmm(torch.unsqueeze(ht.t(), 0), torch.unsqueeze(hs, 0))
+        bmm_squeeze = torch.squeeze(bmm, 0)
+        return torch.exp(bmm_squeeze)
+
+    def all_score(self, ht, list_hs):
+        all_score = 0
+        for hs in  list_hs:
+            all_score += self.each_score(ht, hs)
+        return all_score
+
+    def a_t(self, ht, hs ,list_hs):
+        each_score = self.each_score(ht, hs)
+        all_score = self.all_score(ht, list_hs)
+        return each_score / all_score
+
     def forward(self, input_lines ,target_lines):
         global all_loss
 
-        hx = torch.zeros(batch_size, self.hidden_size).cuda()
+        hs = torch.zeros(batch_size, self.hidden_size).cuda()
         cx = torch.zeros(batch_size, self.hidden_size).cuda()
-
+        ct = 0
+        list_hs = []
         for input_sentence_words in input_lines:
             input_k = self.embed_input(input_sentence_words)
-            hx, cx = self.lstm1(input_k, (hx, cx) )
-        
+            hs, cx = self.lstm1(input_k, (hs, cx) )
+            ct += hs
+            list_hs.append(hs)
+
         target_lines_not_last = target_lines[:(padding_num-1)]
         target_lines_next = target_lines[1:]
         for i ,(target_sentence_words , target_sentence_words_next) in enumerate(zip(target_lines_not_last, target_lines_next)):
             if i == 0:
-                accum_loss = F.cross_entropy(self.linear1(hx), target_sentence_words)
+                accum_loss = F.cross_entropy(self.linear1(hs), target_sentence_words)
+                ht = hs
             target_k = self.embed_target(target_sentence_words)
-            hx, cx = self.lstm1(target_k, (hx, cx) )
-            loss = F.cross_entropy(self.linear1(hx), target_sentence_words_next)
+            ht, cx = self.lstm1(target_k, (ht, cx) )
+            at = self.a_t(ht, list_hs[i] ,list_hs)
+            loss = 0
+            break
+            # loss = F.cross_entropy(self.linear1(ht), target_sentence_words_next)
             accum_loss += loss
 
         return accum_loss
@@ -63,7 +87,7 @@ model = model.to(device)
 
 start = time.time()
 
-epoch_num = 20
+epoch_num = 1
 for epoch in range(epoch_num):
     print("epoch",epoch)
     indexes = torch.randperm(train_num)
