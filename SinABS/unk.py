77ad19b360cdebb5b7ddec6a9577f4122f9f8948
imaharu <light1200out@gmail.com>
2018-11-15 11:48:12 +0900

    Do unk file

--- SinABS/unk.py
+++ SinABS/unk.py
@@ -12,7 +12,7 @@ c_vocab = Counter()
 train_doc_num = int(argvs[1]) # defalut 20000
 co_num = int(argvs[2]) # defalut 30000
 data_path = os.environ["cnn_data"]
-data_path = "/home/ochi/Lab/SinABS/test"
+#data_path = "/home/ochi/Lab/SinABS/test"
 
 english_paths = sorted(glob.glob(data_path + "/*.story"))[0:train_doc_num]
 def get_unk_dict(language_files, c_vocab):
@@ -41,13 +41,17 @@ def unk_file(language_files, unk_dict):
         with open(filename) as f:
             doc = []
             for line in f:
+                concat_line = []
                 for word in line.split():
                     if word in unk_dict:
-                        doc.append("<unk>")
+                        concat_line.append("<unk>")
                     else:
-                        doc.append(word)
-        print(doc)
-        exit()
+                        concat_line.append(word)
+                doc.append(" ".join(concat_line))
+        with open("cnn_unk/" + str(i).zfill(5) + "story",  "w" ) as w_f:
+            w_f.write("\n".join(doc))
+        i += 1
+        print(i)
 
 unk_dict = dict(get_unk_dict(english_paths, c_vocab))
 unk_file(english_paths, unk_dict)
