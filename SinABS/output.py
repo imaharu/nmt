a9d9b6380eee7a16b5056d8bfd6c7f3d375a1e9d
imaharu <light1200out@gmail.com>
2018-11-20 17:40:12 +0900

    Wrote output

--- SinABS/output.py
+++ SinABS/output.py
@@ -16,72 +16,49 @@ from define_sin import *
 import time
 
 
-def result(encoder, decoder, source_doc, target_doc):
+def result(encoder, decoder, source_doc):
+    loop_s = 0
+    loop_d = 0
     ew_hx, ew_cx = encoder.w_encoder.initHidden()
     es_hx, es_cx = encoder.s_encoder.initHidden()
-    max_dsn =  max([*map(lambda x: len(x), source_docs )])
-    max_dtn =  max([*map(lambda x: len(x), target_docs )])
+    max_dsn =  max([*map(lambda x: len(x), source_doc )])
     for i in range(0, max_dsn):
-        lines = torch.tensor([ x[i]  for x in source_doc ]).t().cuda(device=device)
-        for words in lines:
-            before_ew_hx , before_ew_cx = ew_hx , ew_cx
-            ew_hx , ew_cx = encoder.w_encoder(words, ew_hx, ew_cx)
-            w_mask = create_mask(words)
-            ew_hx = torch.where(w_mask == 0, before_ew_hx, ew_hx)
-            ew_cx = torch.where(w_mask == 0, before_ew_cx, ew_cx)
-        before_es_hx, before_es_cx = es_hx, es_cx
-        s_mask = create_mask(lines[0])
+        line = torch.tensor([ x[i] for x in source_doc ]).t().cuda(device=device)
+        for word in line:
+            ew_hx , ew_cx = encoder.w_encoder(word, ew_hx, ew_cx)
         es_hx , es_cx = encoder.s_encoder(ew_hx, es_hx, es_cx)
-        es_hx = torch.where(s_mask == 0, before_es_hx, es_hx)
-        es_cx = torch.where(s_mask == 0, before_es_cx, es_cx)
 
     ds_hx, ds_cx = es_hx, es_cx
-
-    for i in range(0, max_dtn):
+    word_id = torch.tensor( [ english_vocab["<bod>"] ]).cuda(device=device)
+    result_d = ""
+    while(int(word_id) != english_vocab["<eod>"] ):
+        loop_w = 0
+        result_s = []
         dw_hx, dw_cx = ds_hx, ds_cx
-        lines = torch.tensor([ x[i]  for x in target_doc ]).t().cuda(device=device)
-        # t -> true, f -> false
-        lines_t_last = lines[1:]
-        lines_f_last = lines[:(len(lines) - 1)]
-        for words_f, word_t in zip(lines_f_last, lines_f_last):
-            before_dw_hx, before_dw_cx = dw_hx, dw_cx
-            dw_hx , dw_cx = decoder.w_decoder(words_f, dw_hx, dw_cx)
-            w_mask = create_mask(words_f)
-            dw_hx = torch.where(w_mask == 0, before_dw_hx, dw_hx)
-            dw_cx = torch.where(w_mask == 0, before_dw_cx, dw_cx)
-            loss += F.cross_entropy(decoder.w_decoder.linear(dw_hx), word_t , ignore_index=0)
-        before_ds_hx, before_ds_cx = ds_hx, ds_cx
-        s_mask = create_mask(lines[0])
-        ds_hx , ds_cx = decoder.s_decoder(ds_hx, dw_hx, dw_cx)
-        ds_hx = torch.where(s_mask == 0, before_ds_hx, ds_hx)
-        ds_cx = torch.where(s_mask == 0, before_ds_cx, ds_cx)
-    return loss
+        if loop_s >= 30:
+            break
+        while(int(word_id) != english_vocab["<teos>"]):
+            dw_hx, dw_cx = decoder.w_decoder(word_id, dw_hx, dw_cx)
+            word_id = torch.tensor([ torch.argmax(decoder.w_decoder.linear(dw_hx), dim=1).data[0]]).cuda(device=device)
+            word = [k for k, v in english_vocab.items() if v == word_id ]
+            result_s.append(word)
+            if loop_w >= 50:
+                break
+            loop_w += 1
+        loop_s += 1
+        print(result_s)
+        ds_hx, ds_cx = decoder.s_decoder(ds_hx, dw_hx, dw_cx)
+    print(result_d)
+    return result_d
 
 if __name__ == '__main__':
-    model.load_state_dict(torch.load("SinABS-10.model"))
     model = HierachicalEncoderDecoder(source_size, target_size, hidden_size).to(device)
+    model.load_state_dict(torch.load("SinABS-10.model"))
     model.eval()
     optimizer = torch.optim.Adam( model.parameters(), weight_decay=0.002)
-
-    for epoch in range(1):
-        target_docs = []
-        source_docs = []
-            source_docs = [ get_source_doc(english_paths[doc_num], english_vocab) for doc_num in indexes[i:i+batch_size]]
-            target_docs = [ get_target_doc(english_paths[doc_num], english_vocab) for doc_num in indexes[i:i+batch_size]]
-            # source_docs
-            max_doc_sentence_num =  max([*map(lambda x: len(x), source_docs )])
-            source_docs = [ [ s + [ english_vocab["<teos>"] ] for s in t_d ] for t_d in source_docs]
-            source_spadding = sentence_padding(source_docs, max_doc_sentence_num)
-            source_wpadding = word_padding(source_spadding, max_doc_sentence_num)
-            for source in source_wpadding:
-                source.append([ english_vocab["<bod>"] ])
-
-            max_doc_target_num =  max([*map(lambda x: len(x), target_docs )])
-            # add <teos> to target_docs
-            target_docs = [ [ s + [ english_vocab["<teos>"] ] for s in t_d ] for t_d in target_docs]
-            target_spadding = sentence_padding(target_docs, max_doc_target_num)
-            target_wpadding = word_padding(target_spadding, max_doc_target_num)
-            for target in target_wpadding:
-                target.append([english_vocab["<eod>"]])
-            retult = result(model.encoder, model.decoder, source_wpadding,target_wpadding)
-
+    for i in range(len(english_paths)):
+        source_doc = [ get_source_doc(english_paths[i], english_vocab) ]
+        source_doc = [ [ s + [ english_vocab["<teos>"] ] for s in t_d ] for t_d in source_doc]
+        result_doc = result(model.encoder, model.decoder, source_doc)
+        print(result_doc)
+        exit()
