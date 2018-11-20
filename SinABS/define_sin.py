cb4f10cc8c99563874fe5104b834fcb74791db50
imaharu <light1200out@gmail.com>
2018-11-20 17:39:48 +0900

    For train

--- SinABS/define_sin.py
+++ SinABS/define_sin.py
@@ -5,19 +5,20 @@ from create_sin_dict import *
 import os
 import glob
 import torch
-
+import pickle
 english_vocab = {}
 
 train_doc_num = 19990
 batch_size = 10
 hidden_size = 512
 
-#data_path = os.environ["cnn_data"]
 data_path = os.environ["cnn_unk"]
-
+#english_paths = sorted(glob.glob(data_path + "/*.story"))[train_doc_num:train_doc_num+10]
 english_paths = sorted(glob.glob(data_path + "/*.story"))[0:train_doc_num]
 
-get_dict(english_paths, english_vocab)
+#get_dict(english_paths, english_vocab)
+with open('cnn.dump', 'rb') as f:
+    english_vocab = pickle.load(f)
 
 source_size = len(english_vocab) + 1
 target_size = len(english_vocab) + 1
