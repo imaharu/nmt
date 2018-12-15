31520d8fcbf3f1a6a4cff618d52ce2ef6ccc5a9a
imaharu <light1200out@gmail.com>
2018-12-15 13:45:58 +0900

    見やすく

---
+++ encoder_decoder/define_variable.py
@@ -0,0 +1,19 @@
+from get_data import *
+train_num, hidden_size, batch_size = 20000, 256, 50
+train_num, hidden_size, batch_size = 10, 4, 2
+
+input_vocab , input_lines, input_lines_number = {}, {}, {}
+target_vocab ,target_lines ,target_lines_number = {}, {}, {}
+output_input_lines = {}
+translate_words = {}
+
+# paddingで0を入れるから
+get_train_data_input(train_num, input_vocab, input_lines_number, input_lines)
+ev = len(input_vocab) + 1
+
+get_train_data_target(train_num, target_vocab, target_lines_number, target_lines, translate_words)
+jv = len(target_vocab) + 1
+
+test_num = 100
+
+get_test_data_target(test_num, output_input_lines)
--- encoder_decoder/eval.py
+++ encoder_decoder/eval.py
@@ -1,71 +1,14 @@
 import torch
 from torch import tensor as tt
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.nn.utils.rnn import *
 import time
-import numpy as np
-from get_data import *
 import torch.optim as optim
+from operator import itemgetter
+from define_variable import *
+from model import *
 import os
-
-train_num, hidden_size= 20000, 256
-test_num = 100
-
-input_vocab , input_lines, input_lines_number = {}, {}, {}
-target_vocab ,target_lines ,target_lines_number = {}, {}, {}
-output_input_lines = {}
-translate_words = {}
-
-get_train_data_input(train_num, input_vocab, input_lines_number, input_lines)
-ev = len(input_vocab) + 1
-
-get_train_data_target(train_num, target_vocab, target_lines_number, target_lines, translate_words)
-jv = len(target_vocab) + 1
-
-get_test_data_target(test_num, output_input_lines)
-
-class EncoderDecoder(nn.Module):
-    def __init__(self, source_size, output_size, hidden_size):
-        super(EncoderDecoder, self).__init__()
-        self.encoder = Encoder(source_size, hidden_size)
-        self.decoder = Decoder(output_size, hidden_size)
-
-class Encoder(nn.Module):
-    def __init__(self, source_size, hidden_size):
-        super(Encoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.source_size = source_size
-        self.embed_source = nn.Embedding(source_size, hidden_size, padding_idx=0)
-        self.drop_source = nn.Dropout(p=0.2)
-        self.lstm_source = nn.LSTMCell(hidden_size, hidden_size)
-
-    def forward(self, sentence_words, hx, cx):
-        source_k = self.embed_source(sentence_words)
-        source_k = self.drop_source(source_k)
-        hx, cx = self.lstm_source(source_k, (hx, cx) )
-        return hx, cx
-
-    def initHidden(self):
-        hx = torch.zeros(1, self.hidden_size).cuda()
-        cx = torch.zeros(1, self.hidden_size).cuda()
-        return hx, cx
-
-class Decoder(nn.Module):
-    def __init__(self, output_size, hidden_size):
-        super(Decoder, self).__init__()
-        self.output_size = output_size
-        self.embed_target = nn.Embedding(output_size, hidden_size, padding_idx=0)
-        self.drop_target = nn.Dropout(p=0.2)
-        self.lstm_target = nn.LSTMCell(hidden_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, output_size)
-
-    def forward(self, target_words, hx, cx):
-        target_k = self.embed_target(target_words)
-        target_k = self.drop_target(target_k)
-        hx, cx = self.lstm_target(target_k, (hx, cx) )
-        return hx, cx
 
 def output(encoder, decoder, output_input_line):
     result = []
