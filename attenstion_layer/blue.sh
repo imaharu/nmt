9e349997159a99bf684c1918792c5090649dea0a
imaharu <light1200out@gmail.com>
2019-01-04 14:26:54 +0900

    Delete directory

--- attenstion_layer/blue.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-correct="../train_data/test.ja"
-layer=$1
-echo "3 layer"
-~/smt/mosesdecoder/scripts/generic/multi-bleu.perl $correct < $layer
--- layer_seq/blue.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-correct="../train_data/test.ja"
-layer=$1
-#echo "1 layer"
-#~/smt/mosesdecoder/scripts/generic/multi-bleu.perl $correct < $result
-#echo "3 layer"
-~/smt/mosesdecoder/scripts/generic/multi-bleu.perl $correct < $layer
