0416bb551826c3e8bfa7ba3ee861f8ac892eb083
imaharu <light1200out@gmail.com>
2019-01-11 16:42:31 +0900

    Add coverage

---
+++ coverage/decoder.py
@@ -0,0 +1,39 @@
+from define import *
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils.rnn as rnn
+
+class Decoder(nn.Module):
+    def __init__(self, target_size, hidden_size):
+        super(Decoder, self).__init__()
+        self.embed_target = nn.Embedding(target_size, embed_size, padding_idx=0)
+        self.drop_target = nn.Dropout(p=0.2)
+        self.lstm_target = nn.LSTMCell(embed_size, hidden_size)
+        self.linear = nn.Linear(hidden_size, target_size)
+
+    def forward(self, target_words, hx, cx):
+        embed = self.embed_target(target_words)
+        embed = self.drop_target(embed)
+        hx, cx = self.lstm_target(embed, (hx, cx) )
+        return hx, cx
+
+class Attention(nn.Module):
+    def __init__(self, hidden_size):
+        super(Attention, self).__init__()
+        self.W_s = nn.Linear(hidden_size, hidden_size)
+        self.v = nn.Linear(hidden_size, 1)
+        self.linear = nn.Linear(hidden_size * 2, hidden_size)
+
+    def forward(self, decoder_hx, encoder_outputs , encoder_feature , mask_tensor):
+        t_k, b, n = list(encoder_outputs.size())
+        dec_feature = self.W_s(decoder_hx)
+        dec_feature = dec_feature.unsqueeze(0).expand(t_k, b, n)
+        att_features = encoder_feature + dec_feature
+        e = torch.tanh(att_features)
+        scores = self.v(e)
+        attn_dist = torch.softmax(scores, dim=0) * mask_tensor
+        content_vector = (attn_dist * encoder_outputs).sum(0)
+        concat = torch.cat((content_vector, decoder_hx), 1)
+        hx_attention = torch.tanh(self.linear(concat))
+        return hx_attention
---
+++ coverage/train.py
@@ -0,0 +1,77 @@
+import torch
+from torch import tensor as tt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.nn.utils.rnn import *
+import time
+import torch.optim as optim
+from define import *
+from model import *
+from tqdm import tqdm
+from dataset import *
+from evaluate_util import *
+
+def train(model, source, target):
+    loss = model(source=source, target=target, train=True)
+    return loss
+
+if __name__ == '__main__':
+    if args.is_short_data:
+        logger.debug("訓練文書数: " +  str(20000))
+    else:
+        logger.debug("訓練文書数: " +  str(100000))
+    logger.debug("hidden_size: " + str(hidden_size))
+    logger.debug("embed_size: " +  str(embed_size))
+    logger.debug("epoch : " + str(epoch))
+    logger.debug("batch size : " +  str(batch_size))
+
+    start = time.time()
+    device = torch.device('cuda:0')
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+
+    data_set = MyDataset(train_source, train_target)
+    train_iter = DataLoader(data_set, batch_size=batch_size, collate_fn=data_set.collater, shuffle=True)
+
+    val_set = EvaluateDataset(val_source)
+    val_iter = DataLoader(val_set, batch_size=1, collate_fn=val_set.collater)
+
+    model = EncoderDecoder(source_size, target_size, hidden_size).cuda(device=device)
+    model.train()
+    optimizer = torch.optim.Adam( model.parameters(), lr=1e-3, weight_decay=1e-6)
+
+    max_score = 0
+    score = 0
+
+    save_model_dir = "{}/{}".format("trained_model", args.save_path)
+    best_model_dir = "{}/{}".format("trained_model", "best-model")
+
+    calc_blue = Evaluate(target_dict, val=1, gold_sentence_file=val_ja, val_iter=val_iter)
+
+    for epoch in range(args.epoch):
+        print("epoch",epoch + 1)
+        tqdm_desc = "[Epoch{:>3}]".format(epoch + 1)
+        tqdm_bar_format = "{l_bar}{bar}|{n_fmt}/{total_fmt} [{elapsed}<{remaining}]"
+        tqdm_kwargs = {'desc': tqdm_desc, 'smoothing': 0.1, 'ncols': 100,
+                    'bar_format': tqdm_bar_format, 'leave': False}
+
+        model.train()
+        for iters in tqdm(train_iter, **tqdm_kwargs):
+            optimizer.zero_grad()
+            loss = train(model, iters[0].cuda(), iters[1].cuda())
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 2.0)
+            optimizer.step()
+
+#        score = calc_blue.GetBlueScore(model)
+#        print("mac_score: {}".format(max_score))
+#        print("score: {}".format(score))
+#        if max_score < score:
+#            max_score = score
+#            best_model_filename = "{}-epoch{}{}".format(save_model_dir, str(epoch + 1),".model")
+#            torch.save(model.state_dict(), best_model_filename)
+        if (epoch + 1) == args.epoch and args.mode == "train":
+            save_model_filename = save_model_dir + str(epoch + 1) + ".model"
+            torch.save(model.state_dict(), save_model_filename)
+        elapsed_time = time.time() - start
+        print("時間:",elapsed_time / 60.0, "分")
