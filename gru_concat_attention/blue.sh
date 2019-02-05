5d74752dad6729e11923560ba8ef2cc78e6f6be1
imaharu <light1200out@gmail.com>
2019-02-06 00:15:00 +0900

    Delete not important

--- gru_concat_attention/blue.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-correct="../train_data/"
-correct=$correct$1
-output=$2
-~/smt/mosesdecoder/scripts/generic/multi-bleu.perl $correct < $output
--- multi_gpu_attenstion/blue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-correct="../train_data/eval.ja"
-output=$1
-~/smt/mosesdecoder/scripts/generic/multi-bleu.perl $correct < $output
